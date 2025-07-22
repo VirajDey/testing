190e244d4b27ad32cef1f6394c52f4c226361eb5
Viraj Dey <viraj65.dey@gmail.com>
2025-07-22 13:24:32 +0530

    error handling

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Router } = require('itty-router');
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const { SUPABASE_URL, SUPABASE_ANON_KEY } = globalThis;
 const SUPABASE_TABLE = 'users';
 
 const router = Router();
